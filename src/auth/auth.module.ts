d1955f45c4115202f2ac96ededa40b9ddf801679
QuocHuy <quochuy@phutungb2b.com>
2026-01-12 20:40:03 +0700

    12/01/2026

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,28 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './passport/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './passport/jwt.strategy';
 
 @Module({
-  providers: [AuthService, LocalStrategy],
-  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      // Ensure ConfigModule is accessible
+      // Only required if ConfigModule wasn't set to isGlobal: true
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES'),
+        },
+      }),
+    }),
+  ],
+  exports: [AuthService],
 })
 export class AuthModule {}
