16baffad73f8a74b5cf98f0b7056dbf5bf1ef0cd
QuocHuy <quochuy@phutungb2b.com>
2026-01-15 19:45:20 +0700

    15/01/2026

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -38,6 +38,7 @@ export class UsersController {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @Public()
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from './schemas/user.schema';
+import { User, UserDocument } from './schemas/user.schema';
 import mongoose, { Model } from 'mongoose';
 import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: SoftDeleteModel<UserDocument>,
+  ) {}
 
   getHashPassWord = (password: string) => {
     const salt = genSaltSync(10);
@@ -70,6 +73,6 @@ export class UsersService {
     if (!mongoose.Types.ObjectId.isValid(id)) {
       return 'Not found User';
     }
-    return await this.userModel.deleteOne({ _id: id });
+    return await this.userModel.softDelete({ _id: id });
   }
 }
