de9d96482fb23e710a52dd76ba75060370dc4692
QuocHuy <quochuy@phutungb2b.com>
2026-02-06 19:48:48 +0700

    06/02/2026

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { MulterConfigService } from './multer.conig';
 
 @Module({
   controllers: [FilesController],
-  providers: [FilesService]
+  providers: [FilesService],
+  imports: [
+    MulterModule.registerAsync({
+      useClass: MulterConfigService,
+    }),
+  ],
 })
 export class FilesModule {}
