d9aedf6ff4762d64c2ea7f46911bf19270d0b679
QuocHuy <quochuy@phutungb2b.com>
2026-02-18 21:02:04 +0700

    18/02/2026

--- src/resumes/resumes.controller.ts
+++ src/resumes/resumes.controller.ts
@@ -1,34 +1,68 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { ResumesService } from './resumes.service';
-import { CreateResumeDto } from './dto/create-resume.dto';
+import { CreateResumeDto, CreateUserCvDto } from './dto/create-resume.dto';
 import { UpdateResumeDto } from './dto/update-resume.dto';
+import { IUser } from 'src/users/users.interface';
+import { ResponseMessage, User } from 'src/decorator/customize';
 
 @Controller('resumes')
 export class ResumesController {
   constructor(private readonly resumesService: ResumesService) {}
 
   @Post()
-  create(@Body() createResumeDto: CreateResumeDto) {
-    return this.resumesService.create(createResumeDto);
+  @ResponseMessage('Create a new resume')
+  create(
+    @Body()
+    createUserCvDto: CreateUserCvDto,
+    @User() user: IUser,
+  ) {
+    return this.resumesService.create(createUserCvDto, user);
+  }
+
+  @Post('by-user')
+  @ResponseMessage('Get Resumes by User')
+  findUserByResume(@User() user: IUser) {
+    return this.resumesService.findUserByResume(user);
   }
 
   @Get()
-  findAll() {
-    return this.resumesService.findAll();
+  @ResponseMessage('Fetch all resumes with paginate')
+  findAll(
+    @Query('current') page: string,
+    @Query('pageSize') limit: string,
+    @Query() querystring: string,
+  ) {
+    return this.resumesService.findAll(+page, +limit, querystring);
   }
 
   @Get(':id')
+  @ResponseMessage('Fetch a resume by id')
   findOne(@Param('id') id: string) {
-    return this.resumesService.findOne(+id);
+    return this.resumesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateResumeDto: UpdateResumeDto) {
-    return this.resumesService.update(+id, updateResumeDto);
+  @ResponseMessage('Update status resume')
+  update(
+    @Param('id') id: string,
+    @Body() updateResumeDto: UpdateResumeDto,
+    @User() user: IUser,
+  ) {
+    return this.resumesService.update(id, updateResumeDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.resumesService.remove(+id);
+  @ResponseMessage('Delete a resume by id')
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.resumesService.remove(id, user);
   }
 }
--- src/resumes/resumes.service.ts
+++ src/resumes/resumes.service.ts
@@ -1,26 +1,132 @@
 import { Injectable } from '@nestjs/common';
-import { CreateResumeDto } from './dto/create-resume.dto';
+import { CreateResumeDto, CreateUserCvDto } from './dto/create-resume.dto';
 import { UpdateResumeDto } from './dto/update-resume.dto';
+import { IUser } from 'src/users/users.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Resume, ResumeDocument } from './schemas/resume.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import aqp from 'api-query-params';
+import { emit } from 'process';
 
 @Injectable()
 export class ResumesService {
-  create(createResumeDto: CreateResumeDto) {
-    return 'This action adds a new resume';
+  constructor(
+    @InjectModel(Resume.name)
+    private resumeModel: SoftDeleteModel<ResumeDocument>,
+  ) {}
+
+  async create(createUserCvDto: CreateUserCvDto, user: IUser) {
+    let Resume = await this.resumeModel.create({
+      url: createUserCvDto.url,
+      companyId: createUserCvDto.companyId,
+      jobId: createUserCvDto.jobId,
+      email: user.email,
+      userId: user._id,
+      status: 'PENDING',
+      history: [
+        {
+          status: 'PENDING',
+          updatedAt: new Date(),
+          updatedBy: {
+            _id: user._id,
+            email: user.email,
+          },
+        },
+      ],
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
+    return {
+      _id: Resume._id,
+      createdAt: Resume.createdAt,
+    };
   }
 
-  findAll() {
-    return `This action returns all resumes`;
+  async findUserByResume(user: IUser) {
+    return await this.resumeModel.findOne({
+      userId: user._id,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} resume`;
+  async findAll(page: number, limit: number, querystring: string) {
+    const { filter, sort, projection, population } = aqp(querystring);
+    delete filter.current;
+    delete filter.pageSize;
+
+    let offset = (+page - 1) * +limit;
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.resumeModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.resumeModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: page, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages, //tổng số trang với điều kiện query
+        total: totalItems, // tổng số phần tử (số bản ghi)
+      },
+      result, //kết quả query
+    };
   }
 
-  update(id: number, updateResumeDto: UpdateResumeDto) {
-    return `This action updates a #${id} resume`;
+  async findOne(id: string) {
+    return this.resumeModel.findOne({
+      _id: id,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} resume`;
+  async update(id: string, updateResumeDto: UpdateResumeDto, user: IUser) {
+    let updateResume = await this.resumeModel.updateOne(
+      {
+        _id: id,
+      },
+      {
+        status: updateResumeDto.status,
+        updatedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+        $push: {
+          history: {
+            status: updateResumeDto.status,
+            updatedAt: new Date(),
+            updatedBy: {
+              _id: user._id,
+              email: user.email,
+            },
+          },
+        },
+      },
+    );
+    return updateResume;
+  }
+
+  async remove(id: string, user: IUser) {
+    await this.resumeModel.findOne(
+      {
+        _id: id,
+      },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return await this.resumeModel.softDelete({
+      _id: id,
+    });
   }
 }
